d31ceb3f815e5f9b65c9d14fe83b87242162ce24
Alia Robinson <robinsat@rose-hulman.edu>
2016-01-19 21:52:09 -0500

    Added Unit Test Project to Solution

---
+++ SpeedyChef/SpeedyChefUnitTests/TimerUnitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpeedyChef;
+
+namespace SpeedyChefUnitTests
+{
+    [TestClass]
+    public class TimerUnitTest
+    {
+        [TestMethod]
+        public void TestCreateTimer()
+        {
+            RecipeStepTimerHandler handler = new RecipeStepTimerHandler("testTimer", 5);
+            Assert.AreEqual("testTimer", handler.GetTimerName());
+            Assert.AreEqual(5, handler.GetFullTime());
+           
+        }
+    }
+}
